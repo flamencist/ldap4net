9c865d929f3cb3db387356503edd231c8460b61a
flamencist <flamencist@mail.ru>
2018-02-25 00:21:26 +0300

    refactoring

--- LdapForNet/ILdapConnection.cs
+++ LdapForNet/ILdapConnection.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using static LdapForNet.Native.Native;
 
 namespace LdapForNet
 {
     public interface ILdapConnection : IDisposable
     {
-        void Connect(string hostname, int port = (int)Native.Native.LdapPort.LDAP, Native.Native.LdapVersion version = Native.Native.LdapVersion.LDAP_VERSION3);
-        void Bind(string mechanism = Native.Native.LdapAuthMechanism.GSSAPI, string userDn = null, string password = null);
-        void SetOption(Native.Native.LdapOption option, int value);
-        void SetOption(Native.Native.LdapOption option, string value);
-        void SetOption(Native.Native.LdapOption option, IntPtr valuePtr);
-        IList<LdapEntry> Search(string @base, string filter, Native.Native.LdapSearchScope scope = Native.Native.LdapSearchScope.LDAP_SCOPE_SUBTREE);
+        void Connect(string hostname, int port = (int)LdapPort.LDAP, LdapVersion version = LdapVersion.LDAP_VERSION3);
+        void Bind(string mechanism = LdapAuthMechanism.GSSAPI, string userDn = null, string password = null);
+        void SetOption(LdapOption option, int value);
+        void SetOption(LdapOption option, string value);
+        void SetOption(LdapOption option, IntPtr valuePtr);
+        IList<LdapEntry> Search(string @base, string filter, LdapSearchScope scope = LdapSearchScope.LDAP_SCOPE_SUBTREE);
     }
 }
