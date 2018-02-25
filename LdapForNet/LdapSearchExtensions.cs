b1942cf7ffc8b5cbd340f1b5224c6064cfdefcd6
Alexander Chermyanin <AChermyanin@os33.os33.com>
2018-02-25 20:49:25 +0000

    add hexescaper

--- LdapForNet/LdapSearchExtensions.cs
+++ LdapForNet/LdapSearchExtensions.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections.Generic;
 using LdapForNet.Utils;
+using static LdapForNet.Native.Native;
 
 namespace LdapForNet
 {
     public static class LdapSearchExtensions
     {
-        public static IList<LdapEntry> SearchByCn(this ILdapConnection connection, string @base, string cn)
+        public static IList<LdapEntry> SearchByCn(this ILdapConnection connection, string @base, string cn, LdapSearchScope scope = LdapSearchScope.LDAP_SCOPE_SUBTREE )
         {
-            return connection.Search(@base, $"(cn={cn})");
+            return connection.Search(@base, $"(cn={cn})",scope);
         }
         
         public static IList<LdapEntry> SearchByCn(this ILdapConnection connection, string cn)
@@ -15,9 +16,10 @@ namespace LdapForNet
             return connection.SearchByCn(LdapUtils.GetDnFromHostname(), cn);
         }
 
-        public static IList<LdapEntry> SearchBySid(this ILdapConnection connection,string @base, string sid)
+        public static IList<LdapEntry> SearchBySid(this ILdapConnection connection,string @base, string sid, LdapSearchScope scope = LdapSearchScope.LDAP_SCOPE_SUBTREE)
         {
-            return connection.Search(@base, $"(objectSID={LdapSidConverter.ConvertToHex(sid)})");
+            var hex = HexEscaper.Escape(LdapSidConverter.ConvertToHex(sid));
+            return connection.Search(@base, $"(objectSID={hex})", scope);
         }
         
         public static IList<LdapEntry> SearchBySid(this ILdapConnection connection, string sid)
