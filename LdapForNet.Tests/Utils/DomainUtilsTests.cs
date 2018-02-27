3dc6517f98cb6ea45c3c84c1f1edce7b4f7abc15
flamencist <flamencist@mail.ru>
2018-02-27 22:01:19 +0300

    add LdapConnection.Connect without arguments

---
+++ LdapForNet.Tests/Utils/DomainUtilsTests.cs
@@ -0,0 +1,23 @@
+﻿using LdapForNet.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LdapForNetTests.Utils
+{
+    [TestClass]
+    public class DomainUtilsTests
+    {
+        [TestMethod]
+        public void DomainUtils_GetDomainFromHostname()
+        {
+            var actual = DomainUtils.GetDomainFromHostname("mycomp.v123.example.com");
+            Assert.AreEqual("v123.example.com", actual);
+        }
+        
+        [TestMethod]
+        public void DomainUtils_GetDomainFromHostname_Return_Hostname_If_Not_Found_Domain()
+        {
+            var actual = DomainUtils.GetDomainFromHostname("mycomp");
+            Assert.AreEqual("mycomp", actual);
+        }
+    }
+}
