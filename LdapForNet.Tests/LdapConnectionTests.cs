64814c0e8ae63b8d237dba05e28a013bcdd09d0e
Alexander Chermyanin <flamencist@mail.ru>
2018-06-13 01:57:48 +0300

    add tests on ldap entry add\delete

--- LdapForNet.Tests/LdapConnectionTests.cs
+++ LdapForNet.Tests/LdapConnectionTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using LdapForNet;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using static LdapForNet.Native.Native;
@@ -21,9 +22,24 @@ namespace LdapForNetTests
                 Assert.AreEqual("Read Only Admin", entries[0].Attributes["sn"][0]);
                 Assert.AreEqual("read-only-admin", entries[0].Attributes["cn"][0]);
                 Assert.IsTrue(entries[0].Attributes["objectClass"].Any());
+            }
+        }
+        
+        [TestMethod]
+        public void LdapConnection_Search_Return_LdapEntries_List2()
+        {
+            using (var connection = new LdapConnection())
+            {
+                connection.Connect(Config.LdapHost2,Config.LdapPort2);
+                connection.Bind(LdapAuthMechanism.SIMPLE,Config.LdapUserDn2, Config.LdapPassword2);
+                var entries = connection.Search(Config.RootDn2, "(&(objectclass=top)(cn=admin))");
+                Assert.IsTrue(entries.Count == 1);
+                Assert.AreEqual(Config.LdapUserDn2, entries[0].Dn);
+                Assert.AreEqual("admin", entries[0].Attributes["cn"][0]);
                 Assert.IsTrue(entries[0].Attributes["objectClass"].Any());
             }
         }
+
         
         [TestMethod]
         [ExpectedException(typeof(LdapException))]
@@ -67,6 +83,51 @@ namespace LdapForNetTests
             {
                 connection.Connect(Config.LdapHost);
                 connection.Search("dc=example,dc=com", "(&(objectclass=top)...wrong...)");
+            }
+        }
+
+
+        [TestMethod]
+        public void LdapConnection_Add_Delete()
+        {
+            DeleteLdapEntry();
+            AddLdapEntry();
+            DeleteLdapEntry();
+        }
+
+        private static void AddLdapEntry()
+        {
+            using (var connection = new LdapConnection())
+            {
+                connection.Connect(Config.LdapHost2, Config.LdapPort2);
+                connection.Bind(LdapAuthMechanism.SIMPLE, Config.LdapUserDn2, Config.LdapPassword2);
+                connection.Add(new LdapEntry
+                {
+                    Dn = $"cn=test,{Config.RootDn2}",
+                    Attributes = new Dictionary<string, List<string>>
+                    {
+                        {"sn", new List<string> {"Winston"}},
+                        {"objectclass", new List<string> {"inetOrgPerson"}},
+                        {"givenName", new List<string> {"test_value"}}
+                    }
+                });
+                var entries = connection.Search(Config.RootDn2, "(&(objectclass=top)(cn=test))");
+                Assert.IsTrue(entries.Count == 1);
+                Assert.AreEqual($"cn=test,{Config.RootDn2}", entries[0].Dn);
+                Assert.AreEqual("test_value", entries[0].Attributes["givenName"][0]);
+                Assert.IsTrue(entries[0].Attributes["objectClass"].Any());
+            }
+        }
+
+        private static void DeleteLdapEntry()
+        {
+            using (var connection = new LdapConnection())
+            {
+                connection.Connect(Config.LdapHost2, Config.LdapPort2);
+                connection.Bind(LdapAuthMechanism.SIMPLE, Config.LdapUserDn2, Config.LdapPassword2);
+                connection.Delete($"cn=test,{Config.RootDn2}");
+                var entries = connection.Search(Config.RootDn2, "(&(objectclass=top)(cn=test))");
+                Assert.IsTrue(entries.Count == 0);
             }
         }
     }
