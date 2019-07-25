a3af26390edbfccbabd649fe34f3631a72dd9b31
Alexander Chermyanin <flamencist@mail.ru>
2019-07-25 10:32:22 +0000

    try fix build

--- LdapForNet/Native/Native.cs
+++ LdapForNet/Native/Native.cs
@@ -7,7 +7,7 @@ namespace LdapForNet.Native
     public static partial class Native
     {
 //        private const string LIB_LDAP_PATH = "ldap-2.4.so.2";
-        private const string LIB_LDAP_PATH = " /usr/local/opt/openldap/lib/libldap-2.4.2.dylib";
+        private const string LIB_LDAP_PATH = " /usr/local/opt/openldap/lib/libldap.dylib";
         public delegate int LDAP_SASL_INTERACT_PROC(IntPtr ld, uint flags, IntPtr defaults, IntPtr interact);
 
         [DllImport(LIB_LDAP_PATH)]
