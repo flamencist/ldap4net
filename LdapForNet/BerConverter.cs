9cd3943d2b340bc45fdc0df16d5c5dd0ef58591a
Alexander Chermyanin <achermyanin@os33.com>
2020-01-25 18:37:14 +0000

    cleanup

--- LdapForNet/BerConverter.cs
+++ LdapForNet/BerConverter.cs
@@ -94,7 +94,7 @@ namespace LdapForNet
         private static readonly IDictionary<char, BerDecodeAction> DecodeActions = new Dictionary<char, BerDecodeAction>
         {
             ['a'] = new BerDecodeAction(BerScanfStringFromByteArray),
-            ['A'] = new BerDecodeAction(BerScanfStringFromByteArray,false,'a'),
+            ['A'] = new BerDecodeAction(BerScanfStringFromByteArray,'a'),
             ['O'] = new BerDecodeAction(BerScanfByteArray),
             ['b'] = new BerDecodeAction(BerScanfInt),
             ['e'] = new BerDecodeAction(BerScanfInt),
@@ -109,9 +109,9 @@ namespace LdapForNet
             ['}'] = new BerDecodeAction(BerScanfEmptyTag, true),
             ['['] = new BerDecodeAction(BerScanfEmptyTag, true),
             [']'] = new BerDecodeAction(BerScanfEmptyTag, true),
-            ['s'] = new BerDecodeAction(BerScanfStringFromByteArray, false,'a'),
-            ['o'] = new BerDecodeAction(BerScanfByteArray, false, 'O'),
-            ['m'] = new BerDecodeAction(BerScanfByteArray, false, 'O'),
+            ['s'] = new BerDecodeAction(BerScanfStringFromByteArray, 'a'),
+            ['o'] = new BerDecodeAction(BerScanfByteArray,  'O'),
+            ['m'] = new BerDecodeAction(BerScanfByteArray,  'O'),
             ['W'] = new BerDecodeAction(BerScanfBerValMultiByteArrayW),
         };
 
