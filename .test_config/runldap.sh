8d78f216b89a750db276ecbe7324ad6d4b9c17ff
Alexander Chermyanin <flamencist@mail.ru>
2020-03-19 21:12:54 +0300

    configure digest-md5 and proxy authorization

--- .test_config/runldap.sh
+++ .test_config/runldap.sh
@@ -5,8 +5,11 @@ TMPDIR="/tmp/slapd"
 if [ ! -d "${TMPDIR}" ]
 then
   mkdir -p "${TMPDIR}"
+else
+  rm -rf "${TMPDIR}"
+  mkdir -p "${TMPDIR}"
 fi
 
-slapd -f "${DIRNAME}/slapd.linux.conf" -h ldap://localhost:4389 -d 4 &
+slapd -f "${DIRNAME}/slapd.linux.conf" -h ldap://localhost:4389 -d 256 &
 sleep 6
 ldapadd -h localhost:4389 -D cn=admin,dc=example,dc=com -w test -f "${DIRNAME}/base.ldif"
