4cc810555b6f51c720d57cdefae14b66172eab08
Ales Jeusnik <ales.jeusnik@inel.com>
2022-11-24 15:21:17 +0100

    Add search with paging extensions.

--- LdapForNet/LdapSearchExtensions.cs
+++ LdapForNet/LdapSearchExtensions.cs
@@ -77,5 +77,62 @@ namespace LdapForNet
         {
             return await connection.SearchBySidAsync(LdapUtils.GetDnFromHostname(), sid);
         }
+
+        public static IList<LdapEntry> SearchWithPaging(
+            this ILdapConnection connection,
+            SearchRequest request,
+            int pageSize = 1000)
+        {
+            var results = new List<DirectoryEntry>();
+            var resultRequestControl = new PageResultRequestControl(pageSize);
+            request.Controls.Add(resultRequestControl);
+
+            var response = (SearchResponse)connection.SendRequest(request);
+            results.AddRange(response.Entries);
+
+            PageResultResponseControl pageResultResponseControl;
+            while (true)
+            {
+                pageResultResponseControl = (PageResultResponseControl)response.Controls.FirstOrDefault(_ => _ is PageResultResponseControl);
+                if (pageResultResponseControl == null || pageResultResponseControl.Cookie.Length == 0)
+                {
+                    break;
+                }
+
+                resultRequestControl.Cookie = pageResultResponseControl.Cookie;
+                response = (SearchResponse)connection.SendRequest(request);
+                results.AddRange(response.Entries);
+            }
+            return results.ConvertAll(x => x.ToLdapEntry());
+        }
+
+        public static async Task<IList<LdapEntry>> SearchWithPagingAsync(
+            this ILdapConnection connection,
+            SearchRequest request,
+            int pageSize = 1000,
+            CancellationToken token = default)
+        {
+            var results = new List<DirectoryEntry>();
+            var resultRequestControl = new PageResultRequestControl(pageSize);
+            request.Controls.Add(resultRequestControl);
+
+            var response = (SearchResponse)await connection.SendRequestAsync(request, token);
+            results.AddRange(response.Entries);
+
+            PageResultResponseControl pageResultResponseControl;
+            while (true)
+            {
+                pageResultResponseControl = (PageResultResponseControl)response.Controls.FirstOrDefault(_ => _ is PageResultResponseControl);
+                if (pageResultResponseControl == null || pageResultResponseControl.Cookie.Length == 0)
+                {
+                    break;
+                }
+
+                resultRequestControl.Cookie = pageResultResponseControl.Cookie;
+                response = (SearchResponse)await connection.SendRequestAsync(request, token);
+                results.AddRange(response.Entries);
+            }
+            return results.ConvertAll(x => x.ToLdapEntry());
+        }
     }
 }
